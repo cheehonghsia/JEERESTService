7b84d8eeee0195b2e56747832b8817455cdc9a27
Cheehong Hsia <cheehong@zazu-it.com>
2015-12-07 23:41:05 +0100

    Added

---
+++ src/main/java/com/zazu/dao/PersonDaoImpl.java
@@ -0,0 +1,25 @@
+package com.zazu.dao;
+
+import com.zazu.model.Person;
+
+public class PersonDaoImpl implements PersonDao {
+
+	@Override
+	public void insertPerson(Person person) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Person getPersonById(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getPersonCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
